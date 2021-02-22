7adb18c466e9cc0c158ae2baea8b7de7e83011d0
Azik Memmedli <a.memmedli111@gmail.com>
2021-02-22 18:36:11 +0400

    PragmatechFoundationPrject

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -36,15 +36,15 @@ function Accordion(){
     for(let i=0;i<document.forms['userForm'].elements.length -1;i++){
         if(document.forms['userForm'].elements[0].value==""){
             document.forms['userForm'].elements[0].style.borderBottomColor="red"
-            document.querySelector('.group-btn').nextElementSibling.className='form-output open'
+            document.querySelector('.group-btn').nextElementSibling.className='form-output form-output-open'
         }
         if(document.forms['userForm'].elements[1].value==""){
             document.forms['userForm'].elements[1].style.borderBottomColor="red"
-            document.querySelector('.group-btn').nextElementSibling.className='form-output open'
+            document.querySelector('.group-btn').nextElementSibling.className='form-output form-output-open'
         }
         if(document.forms['userForm'].elements[2].value==""){
             document.forms['userForm'].elements[2].style.borderBottomColor="red"
-            document.querySelector('.group-btn').nextElementSibling.className='form-output open'
+            document.querySelector('.group-btn').nextElementSibling.className='form-output form-output-open'
         }
         else{
             document.forms['userForm'].elements[i].style.borderBottomColor="#585d65"
@@ -52,4 +52,80 @@ function Accordion(){
             document.querySelector('.form-output').nextElementSibling.className='contact-output open'
         }
     }
+}
+
+// // Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+let slayt=document.getElementsByClassName("slayt")
+
+let slaytSayisi=slayt.length
+
+let slaytNo= 0;
+
+slaytGoster(slaytNo)
+
+function onceki(){
+
+    slaytNo--
+
+    slaytGoster(slaytNo)
+
+}
+
+function sonraki(){
+
+    slaytNo++
+
+    slaytGoster(slaytNo)
+}
+
+function slaytGoster(slaytNumarasi){
+
+    slaytNo=slaytNumarasi;
+
+    if(slaytNumarasi >=slaytSayisi){
+
+        slaytNo= 0;
+    }
+
+    if(slaytNumarasi <0){
+
+        slaytNo=slaytSayisi -1;
+    }
+
+    for(i= 0;i <slaytSayisi;i++){
+
+        slayt[i].style.display= "none"
+    }
+
+    slayt[slaytNo].style.display= "block"
+}
+
+function Popup(){
+    event.preventDefault()
+    document.querySelector('.page').nextElementSibling.className='modal modal-open'
 }
