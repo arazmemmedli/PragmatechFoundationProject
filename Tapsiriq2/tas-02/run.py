f1603e950f253b05b29f1483cde3e9e354d8f01d
Azik Memmedli <a.memmedli111@gmail.com>
2021-02-22 09:01:38 +0400

    Tapsiriq2

---
+++ Tapsiriq2/tas-02/run.py
@@ -0,0 +1,55 @@
+myList = [1, 34, 56, 100, -12, 87, 987, 1, 3, 5, 56, 67]
+#Listin içindəki elemənlərin sayını ekrana çap edin
+#print(len(myList))
+
+#Listin eleməntlərini ekrana çap edin
+#print(myList)
+
+#Listin içində cüt yerdə duran elementləri ekrana çap edin
+#for i in range(len(myList)):
+    #if i % 2 ==0:
+        #print(myList[i])
+
+#Listin elementlərini azalan sıra ilə sıralayaraq ekrana çap edin
+#print(sorted(myList, reverse=True))
+
+#Listin içində təkrarlanan elementləri tapın
+#for i in range(len(myList)):
+    #for k in range(i + 1, len(myList)):
+        #if myList[i] == myList[k]:
+            #print(myList[k])
+
+#Listi tərs çevirərək ekrana çap edin
+#for a in reversed(myList):
+    #print(a)
+
+#List elementlərinin cəmini tapın
+#1-ci usul
+#print(sum(myList))
+
+#2-ci usul
+#total=0
+
+#for a in myList:
+    #total=total+a
+#print(total)
+
+#Listin içindəki ən kiçik elementi tapın
+#1-ci usul
+#print(min(myList))
+
+#2-ci usul
+#myList.sort()
+#print(myList[0])
+
+#Listin içindəki ən böyük elementi tapın
+#1-ci usul
+#print(max(myList))
+
+#2-ci usul
+#myList.sort()
+#print(myList[-1])
+
+
+
+
