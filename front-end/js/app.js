e2c7ff66ccbd645e66713b555e6ed95394cce71a
Azik Memmedli <a.memmedli111@gmail.com>
2021-03-16 11:07:40 +0400

    PragmatechFoundationProject

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -31,27 +31,88 @@ function slideShow(class1,class2,class3,px,btn,figur){
     }
 }
 
-function Accordion(){
-    event.preventDefault()
-    for(let i=0;i<document.forms['userForm'].elements.length -1;i++){
-        if(document.forms['userForm'].elements[0].value==""){
-            document.forms['userForm'].elements[0].style.borderBottomColor="red"
-            document.querySelector('.group-btn').nextElementSibling.className='form-output form-output-open'
-        }
-        if(document.forms['userForm'].elements[1].value==""){
-            document.forms['userForm'].elements[1].style.borderBottomColor="red"
-            document.querySelector('.group-btn').nextElementSibling.className='form-output form-output-open'
-        }
-        if(document.forms['userForm'].elements[2].value==""){
-            document.forms['userForm'].elements[2].style.borderBottomColor="red"
-            document.querySelector('.group-btn').nextElementSibling.className='form-output form-output-open'
-        }
-        else{
-            document.forms['userForm'].elements[i].style.borderBottomColor="#585d65"
-            document.querySelector('.group-btn').nextElementSibling.className='form-output close'
-            document.querySelector('.form-output').nextElementSibling.className='contact-output open'
-        }
-    }
+var el = document.getElementById('graph'); // get canvas
+
+var options = {
+    percent:  el.getAttribute('data-percent') || 25,
+    size: el.getAttribute('data-size') || 90,
+    lineWidth: el.getAttribute('data-line') || 4,
+    rotate: el.getAttribute('data-rotate') || 0
+}
+
+var canvas = document.createElement('canvas');
+var span = document.createElement('span');
+span.textContent = options.percent + '%';
+
+if (typeof(G_vmlCanvasManager) !== 'undefined') {
+    G_vmlCanvasManager.initElement(canvas);
+}
+
+var ctx = canvas.getContext('2d');
+canvas.width = canvas.height = options.size;
+
+el.appendChild(span);
+el.appendChild(canvas);
+
+ctx.translate(options.size / 2, options.size / 2); // change center
+ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI); // rotate -90 deg
+
+//imd = ctx.getImageData(0, 0, 240, 240);
+var radius = (options.size - options.lineWidth) / 2;
+
+var drawCircle = function(color, lineWidth, percent) {
+        percent = Math.min(Math.max(0, percent || 1), 1);
+        ctx.beginPath();
+        ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
+        ctx.strokeStyle = color;
+        ctx.lineCap = 'round'; // butt, round or square
+        ctx.lineWidth = lineWidth
+        ctx.stroke();
+};
+
+// drawCircle('#efefef', options.lineWidth, 100 / 100);
+drawCircle('#68e0cf', options.lineWidth, options.percent / 100);
+
+
+// function Accordion(){
+//     event.preventDefault()
+//     for(let i=0;i<document.forms['userForm'].elements.length -1;i++){
+//         if(document.forms['userForm'].elements[0].value==""){
+//             document.forms['userForm'].elements[0].style.borderBottomColor="red"
+//             document.querySelector('.group-btn').nextElementSibling.className='form-output form-output-open'
+//         }
+//         if(document.forms['userForm'].elements[1].value==""){
+//             document.forms['userForm'].elements[1].style.borderBottomColor="red"
+//             document.querySelector('.group-btn').nextElementSibling.className='form-output form-output-open'
+//         }
+//         if(document.forms['userForm'].elements[2].value==""){
+//             document.forms['userForm'].elements[2].style.borderBottomColor="red"
+//             document.querySelector('.group-btn').nextElementSibling.className='form-output form-output-open'
+//         }
+//         else{
+//             document.forms['userForm'].elements[i].style.borderBottomColor="#585d65"
+//             document.querySelector('.group-btn').nextElementSibling.className='form-output close'
+//             document.querySelector('.form-output').nextElementSibling.className='contact-output open'
+//         }
+//     }
+// }
+
+let form=document.forms.userForm;
+
+form.onsubmit = function() {
+  event.preventDefault()
+  if (form.name.value == "" || form.email.value == "" || form.message.value == ""){
+    form.name.style.borderBottomColor = "red"
+    form.email.style.borderBottomColor = "red"
+    form.message.style.borderBottomColor = "red"
+    document.querySelector('.group-btn').nextElementSibling.className='form-output form-output-open'   
+  }else {
+    form.name.style.borderBottomColor = "#585d65"
+    form.email.style.borderBottomColor = "#585d65"
+    form.message.style.borderBottomColor = "#585d65" 
+    document.querySelector('.group-btn').nextElementSibling.className='form-output close'
+    document.querySelector('.form-output').nextElementSibling.className='contact-output open'
+  }
 }
 
 var slideIndex = 1;
